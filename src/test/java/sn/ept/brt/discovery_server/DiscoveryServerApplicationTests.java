a6e8ca1db0b442c5d68fec4b23097365f5054a9e
Absa550 <dndeyeabsa@ept.sn>
2026-04-07 14:05:59 +0200

    Initial commit - discovery service

---
+++ src/test/java/sn/ept/brt/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.ept.brt.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
